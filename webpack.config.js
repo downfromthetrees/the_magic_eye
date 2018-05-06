cda78c551a8724075053f891eb28afd4f3f9d338
nothingformymum@gmail.com <nothingformymum@gmail.com>
2018-05-06 21:25:12 +1200

    Errors only on the webpack output

--- webpack.config.js
+++ webpack.config.js
@@ -35,21 +35,6 @@ module.exports = {
       ),
       new LiveReloadPlugin()
     ],
-    stats: {
-      colors: true,
-      hash: false,
-      version: false,
-      timings: false,
-      assets: false,
-      chunks: false,
-      modules: false,
-      reasons: false,
-      children: false,
-      source: false,
-      errors: false,
-      errorDetails: false,
-      warnings: false,
-      publicPath: false
-    },
+    stats: "errors-only",
     devtool: 'source-map'
 };
