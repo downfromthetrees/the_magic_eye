d6952a6f047425111aa9b7619396a9251a7b95c4
nothingformymum@gmail.com <nothingformymum@gmail.com>
2018-10-17 21:27:29 +1300

    Add logging to find trouble image

--- src/submission_processor.js
+++ src/submission_processor.js
@@ -22,8 +22,8 @@ const { removeReposts } = require('./processing_modules/submission_modules/image
 
 
 async function processSubmission(submission, masterSettings, database, reddit, activeMode) {
-    log.debug('starting processing for ', submission.id, 'submitted:', new Date(submission.created_utc));
     const subredditName = masterSettings._id;
+    log.info(`[${subredditName}]`, 'Starting processing for ', await printSubmission(submission));
 
     // check if we have already processed submission
     const existingMagicSubmissionById = await database.getMagicSubmissionById(submission.id);
