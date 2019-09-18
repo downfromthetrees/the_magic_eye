be3f0cc7f2337d790d38df66ee791ec165bb4950
nothingformymum@gmail.com <nothingformymum@gmail.com>
2019-09-18 21:09:27 +1200

    Soften wording

--- src/processing_modules/submission_modules/image/precheck/removeUncroppedImages.js
+++ src/processing_modules/submission_modules/image/precheck/removeUncroppedImages.js
@@ -31,7 +31,7 @@ async function removeUncroppedImages(reddit, submission, imageDetails, subSettin
         return false;
     } else if (subSettings.removeUncroppedImages.removeVerticalImages && imageIsVertical(imageDetails)) {
         const removalReason =
-`This image appears to be uncropped (i.e. a vertical cellphone pic). Images posted to this subreddit must be cropped, i.e.:
+`This image appears to be uncropped (i.e. a vertical cellphone pic). Images posted to this subreddit should generally be cropped for rule 7, i.e.:
 
 * [Example of an uncropped image](https://i.imgur.com/XAjzOF0.png)
 * [Example image properly cropped](https://i.imgur.com/qND6Vb1.png)
