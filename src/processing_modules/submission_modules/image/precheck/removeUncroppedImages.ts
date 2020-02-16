51b88cd0fef4f843057f9403d755e45e61b9bcba
nothingformymum@gmail.com <nothingformymum@gmail.com>
2020-02-16 15:07:27 +1300

    Update cropping message

--- src/processing_modules/submission_modules/image/precheck/removeUncroppedImages.ts
+++ src/processing_modules/submission_modules/image/precheck/removeUncroppedImages.ts
@@ -31,10 +31,12 @@ export async function removeUncroppedImages(reddit, submission, imageDetails, su
         return false;
     } else if (subSettings.removeUncroppedImages.removeVerticalImages && imageIsVertical(imageDetails)) {
         const removalReason =
-`This image appears to be uncropped because of it's height/width ratio (i.e. it's a vertical cellphone pic). Images posted to this subreddit should generally be cropped for rule 7, i.e.:
+`This image appears to be uncropped because it's a long image (typically a vertical cellphone pic). Images posted to this subreddit should generally first be cropped to a square (rule 7), i.e.:
 
 * [Example of an uncropped image](https://i.imgur.com/XAjzOF0.png)
 * [Example image properly cropped](https://i.imgur.com/qND6Vb1.png)
+
+Well cropped images look better, and get more upvotes.
 `;
         removePost(submission, removalReason, subSettings, reddit);
     }
