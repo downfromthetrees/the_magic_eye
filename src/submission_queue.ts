b2d0f0b15c99703a0253ea624e0c73671dd8f4fd
nothingformymum@gmail.com <nothingformymum@gmail.com>
2020-03-23 10:11:12 +1300

    Log multi string

--- src/submission_queue.ts
+++ src/submission_queue.ts
@@ -46,6 +46,7 @@ export async function mainQueue() {
                 .map(sub => sub + '+')
                 .join('')
                 .slice(0, -1); // rarepuppers+pics+MEOW_IRL
+                log.info('moddedSubredditsMultiString': moddedSubredditsMultiString);
             const subredditMulti = await reddit.getSubreddit(moddedSubredditsMultiString);
 
             // [HMMM] hmmm only block - get the modqueue as well
