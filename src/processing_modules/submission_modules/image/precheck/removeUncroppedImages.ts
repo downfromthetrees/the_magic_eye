a1badf30fdbb37d0d9dd7b9a2c032c7ebdc4b1fa
nothingformymum@gmail.com <nothingformymum@gmail.com>
2020-04-19 20:07:47 +1200

    Horizontal for uncropped images

--- src/processing_modules/submission_modules/image/precheck/removeUncroppedImages.ts
+++ src/processing_modules/submission_modules/image/precheck/removeUncroppedImages.ts
@@ -16,8 +16,8 @@ export async function removeUncroppedImages(reddit, submission, imageDetails, su
         return true;
     }
 
-    if (imageHasBars(imageDetails)) {
-        log.info(`[${subredditName}]`, 'Image is uncropped - border bars. Removing - removing submission: ', await printSubmission(submission));
+    if (imageHasVerticalBars(imageDetails) && subSettings.removeUncroppedImages.removeVertical !== false) {
+        log.info(`[${subredditName}]`, 'Image is uncropped vertically - border bars. Removing - removing submission: ', await printSubmission(submission));
 
         let removalReason = '';
         if (subSettings.removeUncroppedImages.fullRemovalMessage) {
@@ -29,22 +29,40 @@ export async function removeUncroppedImages(reddit, submission, imageDetails, su
         removePost(submission, removalReason, subSettings, reddit);
         logRemoveUncropped(subredditName, null);
         return false;
-    } else if (subSettings.removeUncroppedImages.removeVerticalImages && imageIsVertical(imageDetails)) {
+    }
+
+    if (subSettings.removeUncroppedImages.removeHorizontal && imageHasHorizontalBars(imageDetails)) {
+        log.info(`[${subredditName}]`, 'Image is uncropped horizontally - border bars. Removing - removing submission: ', await printSubmission(submission));
+
+        let removalReason = '';
+        if (subSettings.removeUncroppedImages.fullRemovalMessage) {
+            removalReason = subSettings.removeUncroppedImages.fullRemovalMessage;
+        } else {
+            removalReason = `This image appears to be uncropped (i.e. black/white bars at the sides). Images must be cropped before posting to this subreddit.`;
+        }
+
+        removePost(submission, removalReason, subSettings, reddit);
+        logRemoveUncropped(subredditName, null);
+        return false;
+    }
+
+    if (subSettings.removeUncroppedImages.removeVerticalImages && imageIsVertical(imageDetails)) {
         const removalReason = `This image appears to be uncropped (i.e. a vertical cellphone pic). Images posted to this subreddit must be cropped, i.e.:
 
 * [Example of an uncropped image](https://i.imgur.com/XAjzOF0.png)
 * [Example image properly cropped](https://i.imgur.com/qND6Vb1.png)
 `;
         removePost(submission, removalReason, subSettings, reddit);
+        return false;
     }
 
     return true;
 }
 
-function imageHasBars(imageDetails) {
+function imageHasVerticalBars(imageDetails) {
     const isSquarish = imageDetails.height < imageDetails.width * 1.2;
 
-    if (isSquarish || imageDetails.trimmedHeight == null || imageDetails.trimmedHeight == null) {
+    if (isSquarish || imageDetails.trimmedHeight == null) {
         // Image is already squarish, not checking for crop
         return false;
     }
@@ -54,4 +72,15 @@ function imageHasBars(imageDetails) {
 
 function imageIsVertical(imageDetails) {
     return imageDetails.height > imageDetails.width * 1.8;
+}
+
+function imageHasHorizontalBars(imageDetails) {
+    const isSquarish = imageDetails.width < imageDetails.height * 1.2;
+
+    if (isSquarish || imageDetails.trimmedWidth == null) {
+        // Image is already squarish, not checking for crop
+        return false;
+    }
+
+    return imageDetails.trimmedWidth / imageDetails.width < 0.81; // https://i.imgur.com/VrL2mGp.png
 }
