34526cabf4653ea77a511976489782ad038561b5
nothingformymum@gmail.com <nothingformymum@gmail.com>
2022-04-10 16:05:41 +1200

    Fail if added wrong

--- src/wiki_utils.ts
+++ src/wiki_utils.ts
@@ -8,7 +8,6 @@ const Validator = require('jsonschema').Validator;
 log.setLevel(process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'info');
 import { getSubredditSettings, setSubredditSettings } from './master_database_manager';
 
-
 export async function createDefaultSettings(subredditName, masterSettings, reddit) {
     log.info(`[${subredditName}]`, 'Creating default settings for', subredditName, '...');
     const wikiPage = await reddit.getSubreddit(subredditName).getWikiPage('magic_eye');
@@ -27,13 +26,25 @@ export async function createDefaultSettings(subredditName, masterSettings, reddi
     const stringSettings = JSON.stringify(masterSettings.settings, null, 4);
     const indentedSettings = indentString(stringSettings, 4);
     try {
-        await wikiPage.edit({text: indentedSettings, reason: 'Create default Magic Eye settings.'});
-        await wikiPage.editSettings({listed: false, permission_level: 2}); // mod only, not listed
+        await wikiPage.edit({ text: indentedSettings, reason: 'Create default Magic Eye settings.' });
+        await wikiPage.editSettings({ listed: false, permission_level: 2 }); // mod only, not listed
         log.info(`[${subredditName}]`, 'Finished creating default settings for', subredditName, '...');
     } catch (e) {
         if (e.message && e.message.includes('WIKI_DISABLED')) {
             throw `[${subredditName}] Cannot create settings because WIKI_DISABLED`;
         } else {
+            log.info('[DEMOD] Failed to create wiki page, Demodding from: ', subredditName);
+            await reddit.getSubreddit(subredditName).leaveModerator();
+
+            await reddit.composeMessage({
+                to: await `/r/${subredditName}`,
+                subject: `Initialisation failed.`,
+                text: outdent`
+                    Hello. It looks like you have failed to add me with the correct permissions. I have demodded, so to fix this you will need to remod me with the correct permissions listed in the documentation:
+                    
+                    https://github.com/downfromthetrees/the_magic_eye/blob/master/README.md#setup
+                    `,
+            });
             throw e;
         }
     }
@@ -46,7 +57,7 @@ export async function writeSettings(subredditName, masterSettings, reddit) {
     const stringSettings = JSON.stringify(masterSettings.settings, null, 4);
     const indentedSettings = indentString(stringSettings, 4);
     try {
-        await wikiPage.edit({text: indentedSettings, reason: 'Updating Magic Eye settings (new settings version)'});
+        await wikiPage.edit({ text: indentedSettings, reason: 'Updating Magic Eye settings (new settings version)' });
         log.info(`[${subredditName}]`, 'Settings upgrade complete for', subredditName);
     } catch (e) {
         if (e.message && e.message.includes('WIKI_DISABLED')) {
@@ -57,10 +68,9 @@ export async function writeSettings(subredditName, masterSettings, reddit) {
     }
 }
 
-
 export async function doUpdateSettings(subredditName, change, reddit) {
     log.info('Updating settings for', subredditName);
-    const subreddit = await reddit.getSubreddit(subredditName); 
+    const subreddit = await reddit.getSubreddit(subredditName);
     const wikiPage = await subreddit.getWikiPage('magic_eye');
     let settings;
     try {
@@ -83,18 +93,16 @@ async function sendSuccessReply(username, reddit, subredditName: string) {
     await reddit.composeMessage({
         to: await username,
         subject: 'Settings update successful',
-        text: `Settings update successful for r/${subredditName}. Let's nuke some posts!`
-      });
+        text: `Settings update successful for r/${subredditName}. Let's nuke some posts!`,
+    });
 }
 
 async function sendFailureReply(username, reddit, subredditName: string) {
     await reddit.composeMessage({
         to: await username,
         subject: 'Settings update failed',
-        text: 
-`The changes you made to the settings for r/${subredditName} aren't formatted right so I haven't updated them.
+        text: `The changes you made to the settings for r/${subredditName} aren't formatted right so I haven't updated them.
         
-Use https://jsonlint.com/ to find the issue (typically a trailing comma, missing comma, or missing quotation marks). Either that or restore the last settings using the wiki page history.`
-      });
+Use https://jsonlint.com/ to find the issue (typically a trailing comma, missing comma, or missing quotation marks). Either that or restore the last settings using the wiki page history.`,
+    });
 }
-
