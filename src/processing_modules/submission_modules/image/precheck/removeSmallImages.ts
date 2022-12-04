ac10d1db2bd7645cd410ee2a7ab70ba83525f567
nothingformymum@gmail.com <nothingformymum@gmail.com>
2022-12-04 20:55:14 +1300

    Had this change sitting here unpushed

--- src/processing_modules/submission_modules/image/precheck/removeSmallImages.ts
+++ src/processing_modules/submission_modules/image/precheck/removeSmallImages.ts
@@ -15,24 +15,24 @@ import { logRemoveSmall } from '../../../../master_stats';
 export async function removeSmallImages(reddit, submission, imageDetails, subSettings, subredditName, submissionType) {
     if (!subSettings.removeSmallImages || submissionType !== 'image') {
         return true;
-    }   
+    }
 
     const smallDimension = subSettings.removeSmallImages.smallDimension;
 
     if (isImageTooSmall(imageDetails, smallDimension)) {
-        log.info(`[${subredditName}]`, "Image is too small, removing - removing submission: ", await printSubmission(submission));
+        log.info(`[${subredditName}]`, 'Image is too small, removing - removing submission: ', await printSubmission(submission));
 
-        let removalReason = "";
+        let removalReason = '';
         if (subSettings.removeSmallImages.fullRemovalMessage) {
-            removalReason = subSettings.removeSmallImages.fullRemovalMessage.split("{{dimension}}").join(smallDimension);
+            removalReason = subSettings.removeSmallImages.fullRemovalMessage.split('{{dimension}}').join(smallDimension);
         } else {
             removalReason = `Your image has been removed because it is too small. Image submissions to this subreddit must be larger than ${smallDimension}px*${smallDimension}px.`;
         }
 
-        removePost(submission, removalReason, subSettings, reddit);
+        removePost(submission, removalReason, subSettings, reddit, subSettings.removeSmallImages.silent);
         logRemoveSmall(subredditName, null);
         return false;
-    }   
+    }
 
     return true;
 }
@@ -42,5 +42,5 @@ function isImageTooSmall(imageDetails, smallDimension) {
         return false;
     }
 
-    return (imageDetails.height * imageDetails.width) < (smallDimension * smallDimension);
+    return imageDetails.height * imageDetails.width < smallDimension * smallDimension;
 }
